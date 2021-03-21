dfbbedec0c7981f0c744f16223bfffb47ec134d2
Phan Mạnh Hiếu <62614519+stewiephan@users.noreply.github.com>
2021-03-21 16:52:46 +0700

    Create WS1-Q2.c

---
+++ WS1-Q2.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(){
+	
+	
+	/* float a,b,c,min,max;
+	scanf("%f%f%f",&a,&c,&b);
+    if (a>=b)
+	printf ("min%f",b);
+    else if (a<=c)
+	printf ("min=%f",a);
+	else
+	printf("min=%f",c);
+    if( (a>b) && (a>c)) {printf("max=%f", a);}
+    else if( (b>c) && (b>c)) {printf("max=%f", b);}
+    else if  ( (c>a) && (c>b)) {printf("max=%f", c);}
+		} */
+	
+	
+	/*int a,b,c;
+	printf ("Enter =",a); scanf("%d",&a);
+	printf ("Enter =",b); scanf("%d",&b);
+	printf ("Enter =",c); scanf("%d",&c);
+    
+    float t;
+    if (a>=b)
+    {t=a; a=b; b=t;}
+    if (a>=c)
+    {t=a; a=c; c=t;}
+    if (b>=c)
+    {t=b; b=c; c=t;}
+    printf("OUTPUT\n");
+    printf("%d\t%d\t%d",a,b,c);
+    return 0;
+    }}*/
